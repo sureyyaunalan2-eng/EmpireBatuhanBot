819489ce4c197b28f796d7ac17dfad55396c09ff
Su <sureyyaunalan2@gmail.com>
2026-08-29 17:02:13 +0300

    http handler duzeltildi

--- empire_bot.py
+++ empire_bot.py
@@ -2,9 +2,16 @@ import os
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import threading
 
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Bot is alive!")
+
 def run_dummy_server():
     port = int(os.environ.get("PORT", 10000))
-    server = HTTPServer(('0.0.0.0', port), BaseHTTPRequestHandler)
+    server = HTTPServer(('0.0.0.0', port), SimpleHandler)
     server.serve_forever()
 
 threading.Thread(target=run_dummy_server, daemon=True).start()
